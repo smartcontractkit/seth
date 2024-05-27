49e502f60c46f68dd80af8b9a1466560e54b6e5b
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-05-27 16:16:10 +0200

    more test fix

--- network_cli_test.go
+++ network_cli_test.go
@@ -1,6 +1,7 @@
 package seth_test
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -10,48 +11,74 @@ import (
 )
 
 func TestCLINetworkFromEnv(t *testing.T) {
+	network := os.Getenv(seth.NETWORK_ENV_VAR)
+	defer func() {
+		_ = os.Setenv(seth.NETWORK_ENV_VAR, network)
+	}()
 	err := os.Unsetenv(seth.NETWORK_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.CHAIN_ID_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.URL_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
-	err = sethcmd.RunCLI([]string{"seth", "-n", "Geth", "keys", "split", "-a", "10", "-b", "10"})
+	err = sethcmd.RunCLI([]string{"seth", "-n", network, "keys", "split", "-a", "10", "-b", "10"})
 	require.NoError(t, err, "Error splitting keys")
 }
 
 func TestCLIDefaultNetwork(t *testing.T) {
-	err := os.Unsetenv(seth.NETWORK_ENV_VAR)
+	url, chainId, err := getUrlAndChainIdFromEnv()
+	require.NoError(t, err, "Error getting URL and chain ID from env")
+	network := os.Getenv(seth.NETWORK_ENV_VAR)
+	defer func() {
+		_ = os.Setenv(seth.NETWORK_ENV_VAR, network)
+	}()
+	err = os.Unsetenv(seth.NETWORK_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
-	err = sethcmd.RunCLI([]string{"seth", "-c", "1337", "-u", "http://localhost:8545", "keys", "split", "-a", "10", "-b", "10"})
+	err = sethcmd.RunCLI([]string{"seth", "-c", chainId, "-u", url, "keys", "split", "-a", "10", "-b", "10"})
 	require.NoError(t, err, "Error splitting keys")
 }
 
 func TestCLIDefaultNetworkNoUrl(t *testing.T) {
-	err := os.Unsetenv(seth.NETWORK_ENV_VAR)
+	_, chainId, err := getUrlAndChainIdFromEnv()
+	require.NoError(t, err, "Error getting URL and chain ID from env")
+	network := os.Getenv(seth.NETWORK_ENV_VAR)
+	defer func() {
+		_ = os.Setenv(seth.NETWORK_ENV_VAR, network)
+	}()
+	err = os.Unsetenv(seth.NETWORK_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.CHAIN_ID_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.URL_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
-	err = sethcmd.RunCLI([]string{"seth", "-c", "1337", "keys", "split", "-a", "10", "-b", "10"})
+	err = sethcmd.RunCLI([]string{"seth", "-c", chainId, "keys", "split", "-a", "10", "-b", "10"})
 	require.Error(t, err, "No error when splitting keys without URL")
 }
 
 func TestCLITestDefaultNetworkNoChainID(t *testing.T) {
-	err := os.Unsetenv(seth.NETWORK_ENV_VAR)
+	url, _, err := getUrlAndChainIdFromEnv()
+	require.NoError(t, err, "Error getting URL and chain ID from env")
+	network := os.Getenv(seth.NETWORK_ENV_VAR)
+	defer func() {
+		_ = os.Setenv(seth.NETWORK_ENV_VAR, network)
+	}()
+	err = os.Unsetenv(seth.NETWORK_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.CHAIN_ID_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.URL_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
-	err = sethcmd.RunCLI([]string{"seth", "-u", "http://localhost:8545", "keys", "split", "-a", "10", "-b", "10"})
+	err = sethcmd.RunCLI([]string{"seth", "-u", url, "keys", "split", "-a", "10", "-b", "10"})
 	require.Error(t, err, "No error when splitting keys without URL")
 }
 
 func TestCLITestDefaultNetworkNoChainIDNoUrlNorNetworkName(t *testing.T) {
 	err := os.Unsetenv(seth.NETWORK_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
+	network := os.Getenv(seth.NETWORK_ENV_VAR)
+	defer func() {
+		_ = os.Setenv(seth.NETWORK_ENV_VAR, network)
+	}()
 	err = os.Unsetenv(seth.URL_ENV_VAR)
 	require.NoError(t, err, "Error unsetting env var")
 	err = os.Unsetenv(seth.CHAIN_ID_ENV_VAR)
@@ -59,4 +86,19 @@ func TestCLITestDefaultNetworkNoChainIDNoUrlNorNetworkName(t *testing.T) {
 
 	err = sethcmd.RunCLI([]string{"seth", "keys", "split", "-a", "10", "-b", "10"})
 	require.Error(t, err, "No error when splitting keys without URL and chain ID")
+}
+
+func getUrlAndChainIdFromEnv() (url, chainId string, err error) {
+	network := os.Getenv(seth.NETWORK_ENV_VAR)
+	switch network {
+	case "Geth":
+		url = "ws://localhost:8546"
+		chainId = "1337"
+	case "Anvil":
+		url = "ws://localhost:8545"
+		chainId = "31337"
+	default:
+		err = fmt.Errorf("unsupported network : %s", network)
+	}
+	return
 }
