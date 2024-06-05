62937264b7f060b7ba44ea11462332f97e5cbcc6
Bartek Tofel <bartek.tofel@smartcontract.com>
2024-06-05 14:47:26 +0200

    add one simple cmd test, fix another one

--- keyfile_cli_test.go
+++ keyfile_cli_test.go
@@ -2,6 +2,7 @@ package seth_test
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"os"
 	"testing"
@@ -14,7 +15,7 @@ import (
 func AssertFileBalances(t *testing.T, amount *big.Int, keyfilePath string) {
 	c := newClient(t)
 	c.Cfg.KeyFilePath = keyfilePath
-	kf, _, err := c.CreateOrUnmarshalKeyFile(nil)
+	kf, _, err := c.CreateOrUnmarshalKeyFile(&seth.FundKeyFileCmdOpts{LocalKeyfile: true})
 	require.NoError(t, err)
 	for _, kfd := range kf.Keys {
 		require.NotEmpty(t, kfd.PrivateKey, "Private key is empty")
@@ -69,4 +70,14 @@ func TestCLIUpdateBalances(t *testing.T) {
 	require.NotEqual(t, kf.Keys[0].Funds, kf.Keys[1].Funds)
 	err = sethcmd.RunCLI([]string{"seth", "-n", os.Getenv(seth.NETWORK_ENV_VAR), "keys", "return", "--local"})
 	require.NoError(t, err)
+}
+
+func TestCLINoVaultPassed(t *testing.T) {
+	keyFilePath := "keyfile_test_2.toml"
+	_ = os.Remove(keyFilePath)
+	err := os.Setenv(seth.KEYFILE_PATH_ENV_VAR, keyFilePath)
+	require.NoError(t, err)
+	err = sethcmd.RunCLI([]string{"seth", "-n", os.Getenv(seth.NETWORK_ENV_VAR), "keys", "fund", "-a", "2", "-b", "10"})
+	require.Error(t, err, "No error when splitting keys without vault id")
+	require.Equal(t, fmt.Sprintf(sethcmd.ErrNo1PassVault, seth.ONE_PASS_VAULT_ENV_VAR), err.Error(), "Error message is incorrect")
 }
